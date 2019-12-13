1ce3aeb9a4c2980f82ea05827c911a66e4983fcc
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2019-12-13 13:49:26 +0100

    fix: add second metric in distributed parse and merge

--- entrypoint_parse.sh
+++ entrypoint_parse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ${YCSB_DIR}
-cp ${MEASUREMENT_RESULTS_DIR}/${PREFIX}*.hdr .
+cp ${MEASUREMENT_RESULTS_DIR}/QUERY_${PREFIX}*.hdr .
+cp ${MEASUREMENT_RESULTS_DIR}/FRESHNESS_LATENCY_${PREFIX}*.hdr .
 ./bin/ycsb load basic -p recordcount=0 -p parse=true -p parsePrefix=${PREFIX} -P workloads/workloada > ${MEASUREMENT_RESULTS_DIR}/${PREFIX}.txt
